2aa981a283fccc4b972bde224c610d338844ad21
bedena2000 <bedena2000@gmail.com>
2022-05-23 23:11:55 +0400

    change: added weather tracker

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
 // Dom elements
-
   // Background Arrows
   let leftArrow = document.querySelector(".left-arrow");
   let rightArrow = document.querySelector(".right-arrow");
@@ -9,6 +8,12 @@
   let musicPlayerRightArrow = document.querySelector(".video-icon-next");
   // Music Player Items
   let musicPlayerItems = Array.from(document.querySelectorAll(".music-list-item"));
+  // Weather Items
+  let weatherInput = document.querySelector(".weather-city");
+  let weatherIcon = document.querySelector(".weather-icon");
+  let weatherClouds = document.querySelector(".weather-clouds");
+  let windSpeed = document.querySelector(".weather-wind-speed");
+  let weatherHumidity = document.querySelector(".weather-humidity");
 // State 
 let randomNumber = Math.floor(Math.random() * 15 + 1);
 let audio = new Audio();
@@ -22,6 +27,14 @@ const data = {
     2: "img/assets/sounds/Summer Wind.mp3",
     3: "img/assets/sounds/Ennio Morricone.mp3"
   },
+  weather: {
+    weatherIconClass: null,
+    weatherTemp: null,
+    weatherClouds: null,
+    weatherWindSpeed: null,
+    weatherHumidity: null
+  },
+  apiKey: "442c6485c22aa2b7df54f85fba75e8b7",
 };
 changeBackgroundImage(data.currentNumber);
 // Video Player Logic
@@ -50,6 +63,32 @@ function musicList() {
 function changeMusic() {
   audio.src = data.musicUrl[data.currentSelectedMusic];
   audio.play();
+};
+async function getPosition(cityName) {
+  try {
+    let geolocation = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=en&appid=${data.apiKey}&units=metric`);
+    let result = await geolocation.json();
+    data.weather.weatherIconClass = result.weather[0].id;
+    data.weather.weatherTemp = result.main.temp;
+    data.weather.weatherClouds = result.weather[0].description;
+    data.weather.weatherWindSpeed = result.wind.speed;
+    data.weather.weatherHumidity = result.main.humidity;
+    weatherIcon.textContent = "";
+    weatherIcon.classList = `weather-icon owf`;
+    weatherIcon.classList.add(`owf-${data.weather.weatherIconClass}`);
+    weatherClouds.textContent = `${Math.floor(data.weather.weatherTemp)}°C`;
+    weatherClouds.textContent = weatherClouds.textContent + ` ${data.weather.weatherClouds}`;
+    windSpeed.textContent = `Wind Speed: ${data.weather.weatherWindSpeed}`;
+    weatherHumidity.textContent = `Humidity: ${data.weather.weatherHumidity}`;
+    console.log(data);
+  } catch (error) {
+    weatherIcon.classList = "";
+    weatherIcon.textContent = `Sorry we do not found anything`;
+    weatherClouds.textContent = "";
+    windSpeed.textContent = "";
+    weatherHumidity.textContent = "";
+    console.log(data);
+  }
 };
 // Click Event
 leftArrow.addEventListener("click", (event) => {
@@ -108,9 +147,19 @@ musicPlayerRightArrow.addEventListener("click", (event) => {
     changeMusic();
   }
 });
-
+weatherInput.addEventListener("change", (event) => {
+  let cityName = event.currentTarget.value.toLowerCase();
+  if(cityName) {
+    getPosition(cityName);
+  }
+});
+weatherInput.addEventListener("submit", (event) => {
+  let cityName = event.currentTarget.value.toLowerCase();
+  if(cityName) {
+    getPosition(cityName);
+  }
+});
 // All Other Functions Execute
-
-
+getPosition("tbilisi");
  
 
